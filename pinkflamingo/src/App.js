4b1a6f436e58f0223ecb321eab054961019c5271
unknown <alexandrannance@gmail.com>
2021-01-12 12:52:44 -0600

    made changes

--- pinkflamingo/src/App.js
+++ pinkflamingo/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+// import Projects from './components/Projects';
+import ContactForm from './components/Contact';
 import './App.css';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'projects',
+      description: 'The compilation of my works',
+    },
+    // { name: 'portraits', description: 'Portraits of people in my life' },
+    // { name: 'food', description: 'Delicious delicacies' },
+    // { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            {/* <Projects currentCategory={currentCategory}></Projects> */}
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          ) }
+      </main>
     </div>
   );
 }
---
+++ pinkflamingo/src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">About Title</h1>
+      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <div className="my-2">
+        <p>
+          Here is where all of the information about me will go!
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About;
